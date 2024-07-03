1f05a5d6fa8dd6135b82578aa1d59cda8a87ceb5
Luiz Gabriel Moraes <luizgmp@id.uff.br>
2024-07-03 13:27:33 -0300

    feat: remocao do arquivo de script da questao 2

--- q2.js
+++
@@ -1,8 +0,0 @@
-function processFormData() {
-   var temp_element;
-   var temp;
-   temp_element = document.getElementById('temp_num'); 
-   temp = temp_element.value;
-   var f = temp * 1.8 + 32;
-   alert("Temperatura em Fahrenheit sera: " + f +" graus");
- }
